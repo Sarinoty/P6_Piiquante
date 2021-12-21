fa9352dbc1c01f472b5d665a5f516fd9d2729d29
Sarinoty <contact@sarinoty.com>
2021-12-21 07:03:35 +0100

    Suppression de emailValidator.

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passwordValidator = require('../middleware/passwordValidator');
 const userController = require('../controllers/user');
-const emailValidator = require('../middleware/emailValidator');
 
-router.post('/signup', emailValidator, passwordValidator, userController.signup);
+router.post('/signup', passwordValidator, userController.signup);
 router.post('/login', userController.login);
 
 module.exports = router;
